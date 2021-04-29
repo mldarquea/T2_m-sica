dee70400a6c159940b43bab0ddd953a2cb87291c
mldarquea <mldarquea@uc.cl>
2021-04-29 16:51:51 -0400

    hello

--- my_app/models.py
+++ my_app/models.py
@@ -1,11 +1,6 @@
-from my_app import db, login_manager
-from flask_login import UserMixin
+from my_app import db
 
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
-
-class User(db.Model, UserMixin):
+class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(80), unique=True, nullable=False)
